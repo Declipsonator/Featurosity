b51673312aa4fa4a97062b88f23d90c8dd37c8e4
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2023-02-09 17:15:17 -0500

    Tuff Stuff

--- src/main/java/me/declipsonator/featurosity/Constants.java
+++ src/main/java/me/declipsonator/featurosity/Constants.java
@@ -1,5 +1,11 @@
 package me.declipsonator.featurosity;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
@@ -8,4 +14,24 @@ public class Constants {
     public static final Identifier FIRECHARGE_THROW_ID = new Identifier("featurosity:firecharge_throw");
     public static SoundEvent FIRECHARGE_THROW_EVENT = SoundEvent.of(FIRECHARGE_THROW_ID);
     public static final double FIRECHARGE_SPEED = 26.9;
+
+    //Tuff Improvements
+    public static final Block POLISHED_TUFF = new Block(AbstractBlock.Settings.of(Material.STONE, MapColor.TERRACOTTA_GRAY).sounds(BlockSoundGroup.TUFF).requiresTool().strength(1.5f, 6.0f));
+    public static final StairsBlock POLISHED_TUFF_STAIRS = new StairsBlock(POLISHED_TUFF.getDefaultState(), AbstractBlock.Settings.copy(POLISHED_TUFF));
+    public static final Block POLISHED_TUFF_WALL = new WallBlock(AbstractBlock.Settings.copy(POLISHED_TUFF));
+    public static final Block POLISHED_TUFF_SLAB = new SlabBlock(AbstractBlock.Settings.copy(POLISHED_TUFF));
+
+    public static final Block TUFF_STAIRS = new StairsBlock(Blocks.TUFF.getDefaultState(), AbstractBlock.Settings.copy(Blocks.TUFF));
+    public static final Block TUFF_WALL = new WallBlock(AbstractBlock.Settings.copy(Blocks.TUFF));
+    public static final Block TUFF_SLAB = new SlabBlock(AbstractBlock.Settings.copy(Blocks.TUFF));
+
+    public static final BlockItem POLISHED_TUFF_ITEM = new BlockItem(POLISHED_TUFF, new Item.Settings());
+    public static final BlockItem POLISHED_TUFF_STAIRS_ITEM = new BlockItem(POLISHED_TUFF_STAIRS, new Item.Settings());
+    public static final BlockItem POLISHED_TUFF_WALL_ITEM = new BlockItem(POLISHED_TUFF_WALL, new Item.Settings());
+    public static final BlockItem POLISHED_TUFF_SLAB_ITEM = new BlockItem(POLISHED_TUFF_SLAB, new Item.Settings());
+
+    public static final BlockItem TUFF_STAIRS_ITEM = new BlockItem(TUFF_STAIRS, new Item.Settings());
+    public static final BlockItem TUFF_WALL_ITEM = new BlockItem(TUFF_WALL, new Item.Settings());
+    public static final BlockItem TUFF_SLAB_ITEM = new BlockItem(TUFF_SLAB, new Item.Settings());
+
 }
--- src/main/java/me/declipsonator/featurosity/Featurosity.java
+++ src/main/java/me/declipsonator/featurosity/Featurosity.java
@@ -1,10 +1,27 @@
 package me.declipsonator.featurosity;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBlockTags;
+import net.fabricmc.fabric.impl.datagen.FabricTagBuilder;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.CryingObsidianBlock;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.registry.tag.TagBuilder;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
 
 public class Featurosity implements ModInitializer {
     public static Logger LOG = LogManager.getLogger("Featurosity");
@@ -13,7 +30,46 @@ public class Featurosity implements ModInitializer {
     public void onInitialize() {
         LOG.info("Initializing Featurosity!");
 
+        //FireCharges
         Registry.register(Registries.SOUND_EVENT, Constants.FIRECHARGE_THROW_ID, Constants.FIRECHARGE_THROW_EVENT);
+
+        //Tuff Stuff
+        Registry.register(Registries.BLOCK, new Identifier("featurosity", "polished_tuff"), Constants.POLISHED_TUFF);
+        Registry.register(Registries.ITEM, new Identifier("featurosity", "polished_tuff"), Constants.POLISHED_TUFF_ITEM);
+
+        Registry.register(Registries.BLOCK, new Identifier("featurosity", "polished_tuff_stairs"), Constants.POLISHED_TUFF_STAIRS);
+        Registry.register(Registries.ITEM, new Identifier("featurosity", "polished_tuff_stairs"), Constants.POLISHED_TUFF_STAIRS_ITEM);
+
+        Registry.register(Registries.BLOCK, new Identifier("featurosity", "polished_tuff_wall"), Constants.POLISHED_TUFF_WALL);
+        Registry.register(Registries.ITEM, new Identifier("featurosity", "polished_tuff_wall"), Constants.POLISHED_TUFF_WALL_ITEM);
+
+        Registry.register(Registries.BLOCK, new Identifier("featurosity", "polished_tuff_slab"), Constants.POLISHED_TUFF_SLAB);
+        Registry.register(Registries.ITEM, new Identifier("featurosity", "polished_tuff_slab"), Constants.POLISHED_TUFF_SLAB_ITEM);
+
+        Registry.register(Registries.BLOCK, new Identifier("featurosity", "tuff_stairs"), Constants.TUFF_STAIRS);
+        Registry.register(Registries.ITEM, new Identifier("featurosity", "tuff_stairs"), Constants.TUFF_STAIRS_ITEM);
+
+        Registry.register(Registries.BLOCK, new Identifier("featurosity", "tuff_wall"), Constants.TUFF_WALL);
+        Registry.register(Registries.ITEM, new Identifier("featurosity", "tuff_wall"), Constants.TUFF_WALL_ITEM);
+
+        Registry.register(Registries.BLOCK, new Identifier("featurosity", "tuff_slab"), Constants.TUFF_SLAB);
+        Registry.register(Registries.ITEM, new Identifier("featurosity", "tuff_slab"), Constants.TUFF_SLAB_ITEM);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
+            content.add(Blocks.TUFF);
+            content.add(Constants.POLISHED_TUFF_ITEM);
+            content.add(Constants.POLISHED_TUFF_STAIRS_ITEM);
+            content.add(Constants.POLISHED_TUFF_WALL_ITEM);
+            content.add(Constants.POLISHED_TUFF_SLAB_ITEM);
+            content.add(Constants.TUFF_STAIRS_ITEM);
+            content.add(Constants.TUFF_WALL_ITEM);
+            content.add(Constants.TUFF_SLAB_ITEM);
+        });
+
+
+
+
+
 
         LOG.info("Initialized Featurosity!");
     }
